40832972ee26721d35866a7e0663dd8c57ca2220
Amirl_kl34 <Amirl_kl34@STHQ0118-04>
2024-09-11 12:01:14 +0400

    Add project files.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client
+{
+    public partial class MainWindow : Window
+    {
+        string sourcePath = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void img_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                sourcePath = openFileDialog.FileName;
+            }
+        }
+        void send_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("Sending started");
+                using (FileStream source = new FileStream(sourcePath, FileMode.Open, FileAccess.Read))
+                {
+
+                    using var client = new TcpClient();
+                    var ip1 = IPAddress.Parse(ip.Text);
+                    var port1 = int.Parse(port.Text);
+                    var endpoint = new IPEndPoint(ip1, port1);
+                    client.Connect(endpoint);
+                    NetworkStream networkStream = client.GetStream();
+                    if (client.Connected)
+                    {
+
+                        try
+                        {
+                            int len = 23;
+                            var bytes = new byte[len];
+                            var fileSize = source.Length;
+                            do
+                            {
+                                len = source.Read(bytes, 0, len);
+                                fileSize -= len;
+                                networkStream.Write(bytes);
+
+                            } while (len > 0);
+
+
+
+                        }
+
+
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Client cant connectt");
+                    }
+
+
+
+
+                }
+
+
+            }
+
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+    }
+}
---
+++ Network Task/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+ 
+
+namespace Network_Task
+{
+     //network
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+
+        void Start()
+        {
+            var port = 27001;
+            var ip = IPAddress.Parse("10.1.18.4");
+            var endPoint = new IPEndPoint(ip, port);
+            using var listener = new TcpListener(endPoint);
+            listener.Start();
+            MessageBox.Show($"Listening on: {listener.LocalEndpoint}");
+            
+            ////////
+            while (true)
+            {
+                var client = listener.AcceptTcpClient();
+                var task = Task.Run(() =>
+                {
+                    Console.WriteLine($"{client.Client.RemoteEndPoint} connected");
+                    var stream = client.GetStream();
+                    var sr = new StreamReader(stream);
+                    while (true)
+                    {
+                        var message = sr.ReadLine();
+                        Console.WriteLine($"{client.Client.RemoteEndPoint}: {message}");
+                    }
+                });
+            }
+            }
+    }
+}
